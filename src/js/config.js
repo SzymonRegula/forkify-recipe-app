76136cb9de546a1773f36c63061e73866b9152bd
SzymonRegula <szymon.regula@gmail.com>
2022-09-13 18:40:59 +0200

    Changed API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '4cf7f450-be5c-436a-be6c-08d6ee818f7a';
+export const KEY = '45a80837-6342-41c0-b365-8d161cb80037';
 export const MODAL_CLOSE_SEC = 1.5;
